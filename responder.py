f5971b468c1ab852d497c93b38874ecb3def5b41
Rybec Thalin Arethdar <rybec@Erisa.rybec.linuxd.org>
2014-11-21 13:05:09 -0700

    Initial commit

---
+++ responder.py
@@ -0,0 +1,272 @@
+import mimetypes
+import os
+
+# Constants
+
+# Files are transferred in chunks of this size.
+# A larger size will probably be faster, but
+# it will also use more memory.
+BUFFER_SIZE = 1024 * 8
+
+
+# Internal globals
+
+# Each element must be a dict of resource keys referencing the response
+# handlers for those resources.
+
+# Response handlers should call set_response() on the response to
+# set the response code, and set_content() to set the content type.
+response_handlers  =  {"GET":{}, "POST":{}}
+
+
+# Functions
+
+# These will populate the response handlers for requests.
+# Currently, other request types can be added manually by adding the
+# appropriate request type to response_handers and populating it with
+# a dict.
+def initGet(handlers):
+	global response_handlers
+	response_handlers["GET"]  = handlers.index
+
+def initPost(handlers):
+	global response_handlers
+	response_handlers["POST"] = handlers.index
+
+
+# These will add handlers for the appropriate request methods.
+# Use these with care, as new handlers can overwrite old ones
+# if there are naming conflicts.
+def addGet(handlers):
+	global response_handlers
+	response_handlers["GET"] = \
+	        dict(response_handlers["GET"].items() + handlers.index.items())
+
+def addPost(handlers):
+	global response_handlers
+	response_handlers["POST"] = \
+	        dict(response_handlers["POST"].items() + handlers.index.items())
+
+
+# Creates and returns the appropriate response type for the request
+def getresponse(request):
+	global response_handlers
+	# Get the resource name
+	resource = request.path.strip("/").split("?", 1)[0].split("/", 1)[0]
+
+	# Check if the first path element is a valid resource
+	if resource in response_handlers[request.command]:
+		# If so, generate a resource response
+		# Note that resources will shadow directories with the
+		# same name.
+		return ResourceResponse(request)
+	else:
+		# Otherwise assume it is a file request
+		return FileResponse(request)
+
+
+
+# Class definitions
+
+# Do not use this abstract class.  Instead inherit or use one of the
+# other included responses.
+class Response(object):
+	# request = BaseHTTPServer.RequestHandler
+	def __init__(self, request):
+		self.request  = request
+		self.response = 200		# Default value
+		self.content  = 'text/html'	# Default value
+
+	def set_response(self, code):
+		if code in self.request.responses:
+			self.response = code
+		else:
+			# Throw some kind of exception here
+			pass
+
+	def set_content(self, contenttype):
+		self.content = contenttype
+
+	# Override this!
+	# This should send the headers and write the data
+	# to the output stream (self.request.wfile.write())
+	def send(self):
+		pass
+
+	# Send error page
+	# send() function should return directly after calling this,
+	# to avoid trying to send anything else
+	# Set reponse code before calling this
+	def send_error(self):
+		error_text = self.request.responses[self.response][0]
+
+		data  = "<!DOCTYPE html>"	# HTML5
+		data += "<html><head><title>"
+		data += error_text
+		data += "</title></head>"
+		data += "<body>"
+		data += "<pre>"
+		data += str(self.response) + ": " + error_text
+		data += "</pre>"
+		data += "</body>"
+		data += "</html>"
+
+
+		# Send HTTP header data
+		self.request.send_response(self.response)
+		self.request.send_header('Content-Type', 'text/html')
+		self.request.send_header('Content-Length', len(data))
+		self.request.end_headers()
+
+		# Send the data
+		self.request.wfile.write(data)
+
+	
+
+# For resource handling (not for file handling)
+class ResourceResponse(Response):
+	# Instance variables
+	# resource = the first element of the path (for "/piano/xyz",
+	#            this would be "piano")
+	# query    = the query string, parsed into a dictionary if possible
+	# subpath  = any elements of the path beyond the first
+
+	def __init__(self, request):
+		# Call parent constructor
+		super(ResourceResponse, self).__init__(request)
+
+		# Parse the URL path
+
+		# Split off the query string
+		if "?" in self.request.path:
+			self.resource, self.query = \
+			        self.request.path.strip("/").split("?", 1)
+
+			# Split the query string first at & and then split
+			# expressions at = and put the result into a dictionary
+			try:
+				self.query = {key:value 
+				              for key, value in [element.split("=")
+				              for element in self.query.split("&")]}
+			except:
+				# We will assume that the malformed query
+				# string was intentional, and we will let
+				# the application handle it
+				pass
+		else:
+			self.resource = self.request.path.strip("/")
+			self.query = None
+
+		# Extract the base resource from the URL
+		# Whatever is left is put in subpath for the user to manage
+		if "/" in self.resource:
+			self.resource, self.subpath = \
+			        self.resource.split("/", 1)
+		else:
+			self.subpath = None
+
+	# This should generate the data, then send the header and then the data
+	def send(self):
+		global response_handlers
+		data = ""
+		try:
+			data = response_handlers[self.request.command][self.resource](self)
+		except KeyError:
+			self.set_response(404)
+			self.send_error()
+			return
+		except Exception as e:
+			print e
+			self.set_response(500)
+			self.send_error()
+			return
+
+		# Send HTTP header data
+		self.request.send_response(self.response)
+		self.request.send_header('Content-Type', self.content)
+		self.request.send_header('Content-Length', len(data))
+		self.request.end_headers()
+
+		# Send the data
+		self.request.wfile.write(data)
+
+
+
+
+# For file handling
+class FileResponse(Response):
+	# Instance variables
+	# path = the cleaned file path
+
+
+	def __init__(self, request):
+		# Call parent constructor
+		super(FileResponse, self).__init__(request)
+
+		# Trim any query string
+		self.path = self.request.path.split("?", 1)[0]
+
+# Python's HTTP module appears to already do this
+# consider this part for deletion
+		# Convert to absolute path (this will remove symbols
+		# like "..", to ensure security)
+		self.path = os.path.abspath(self.path)
+
+		# Find the MIME type and set the content
+		(self.content, _) = mimetypes.guess_type(self.path)
+		if not self.content:
+			self.content = "application/octet-stream"
+
+	# Send the file, or 
+	def send(self):
+		file_size = 0
+		try:
+			file_size = os.path.getsize(os.getcwd() + self.path)
+		except OSError as e:
+			data = ""
+			if e.errno == 2:	# No such file...
+				self.set_response(404)
+				self.send_error()
+			else:			# errno: 13, Permission denied
+				print e
+				self.set_response(403)
+				self.send_error()
+			# We will consider anything that is not 404 to be
+			# Permission denied, even if it is some other
+			# reason.  This will help avoid revealing information
+			# that could be used to compromise security.
+			return
+		except Exception as e:
+			print e
+			# If it is not an OSError, it is probably an
+			# internal server error
+			self.set_response(500)
+			self.send_error()
+			return
+
+		file = None
+		try:
+			file = open(os.getcwd() + self.path)
+
+			# Send HTTP header data
+			self.request.send_response(self.response)
+			self.request.send_header('Content-Type', self.content)
+			self.request.send_header('Content-Length', file_size)
+			self.request.end_headers()
+
+			# Send the file data in chunks of BUFFER_SIZE
+			buffer = file.read(BUFFER_SIZE)
+			while buffer:
+				self.request.wfile.write(buffer)
+				buffer = file.read(BUFFER_SIZE)
+		except Exception as e:
+			print e
+			# We have already checked to make sure the file
+			# exists and is readable.  If we fail now, it is
+			# probably something else.
+			self.set_response(500)
+			self.send_error()
+			return			
+		finally:
+			if file:
+				file.close()
---
+++ rest.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+
+import BaseHTTPServer
+from SocketServer import ThreadingMixIn # Could use ForkingMixIn to multiprocess, but don't
+from os import curdir, sep, chdir, getcwd, path
+import sys
+import imp
+
+import responder
+
+# These are files that should be in webroot/resources/
+gethandlers  = ["gethandlers.py"]
+posthandlers = ["posthandlers.py"]
+
+# This should probably be converted to a config file, or a section
+# in a config file.  (In fact, maybe all of the modules should be
+# listed in a config file.  This might make dynamically loading
+# them easier.)
+shadowhandlers = ["shadowhandlers.py"]
+
+INDEX = "/index.html"
+
+def init():
+	global gethandlers
+	global posthandlers
+	global shadowhandler
+
+	sys.path.append('resources')
+
+	if not path.isdir('resources'):
+		print "No resources directory found, serving only static content"
+		print "Note that this means shadowing is disabled"
+		return
+
+	# Add the shadow handlers first to allow the
+	# get and post handlers to overwrite if necessary
+	for handler in shadowhandlers:
+		name = handler.split('.')[0]
+		mod = imp.load_source(name, "resources/" + handler)
+		responder.addGet(mod)
+		responder.addPost(mod)
+
+	for handler in gethandlers:
+		name = handler.split('.')[0]
+		resource = imp.load_source(name, "resources/" + handler)
+		responder.addGet(resource)
+
+	for handler in posthandlers:
+		name = handler.split('.')[0]
+		resource = imp.load_source(name, "resources/" + handler)
+		responder.addPost(resource)
+
+
+# We need two types of response object, probably in an external
+# module (so that resource handler modules can import them).
+# One will be a dynamically generated response, the other
+# will be exclusively for file data.  The dynamic response
+# will include a string of data to output, while the file
+# response will contain a filename to stream data from (so we
+# do not have to load the entire file into memory).  Using
+# polymorphism, we may be able to include a generator for
+# the output that will handle the streaming transparently.
+
+class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+	# GET request handler
+	def do_GET(self):
+		if self.path == '/':
+			self.path = INDEX
+
+		response = responder.getresponse(self)
+		response.send()
+
+		return
+
+
+	# POST request handler
+	def do_POST(self):
+		# Parse the POST form data
+# This code will need to go in the POST resource handlers.
+#		form = cgi.FieldStorage(
+#			fp      = self.rfile,	# This is where the form data is passed in
+#			headers = self.headers,
+#			environ = {'REQUEST_METHOD':'POST',
+#			           'CONTENT_TYPE':self.headers['Content-Type'],
+#			}
+#		)
+
+		response = responder.getresponse(self)
+		response.send()
+
+
+# This needs to go in a mock POST resource handler.
+		# Just for fun, we are going to return the parsed form data
+#		for key in form.keys():
+#			self.wfile.write('\t%s = %s\n' % (key, form[key].value))
+
+		return
+
+
+class ThreadedHTTPServer(ThreadingMixIn, BaseHTTPServer.HTTPServer):
+	'''
+	    Threaded HTTP Server
+	    Seriously, this is all it takes!
+	'''
+
+def run():
+	httpd = ThreadedHTTPServer(("localhost", 8000), RequestHandler)
+
+	# If we run this in a separate thread, then we can
+	# run some kind of command line interface in the
+	# main thread.  Note that to avoid threading
+	# related issues, we will need to kill the server	
+	# and restart it for certain kinds of changes (like
+	# reloading modules).
+	httpd.serve_forever()
+
+if __name__ == "__main__":
+	if "--webroot" in sys.argv:
+		try:
+			webroot = sys.argv[sys.argv.index("--webroot") + 1]
+			chdir(webroot)
+		except:
+			print "\tInvalid directory"
+			print "\tExample: python webserver.py --webroot /var/www\n"
+			
+			exit()
+
+
+		print "Using directory " + getcwd() + " as webroot\n"
+
+	else:
+		print "This must be run from webroot (the location you want static"
+		print "files to be served from), or it must use the --webroot switch"
+		print "to explicitly set the webroot.\n"
+
+		print "Using current directory, " + getcwd() + " as webroot"
+
+	init()
+
+# Eventually run this in another thread, and present
+# a command line interface for controlling the web
+# server.  (Or even present the CLI without starting
+# the web server, and give the user the chance to
+# configure before starting it.
+	run()
